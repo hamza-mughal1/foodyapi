b0abe1f9ce6f8f5afcb3d2eda41eec328fa2cabf
hamza-gtechnosoft <hamzaxd1123@gmail.com>
2024-11-26 22:18:12 +0500

    Create get_token func to get token from the cookies

--- models/authentication_model.py
+++ models/authentication_model.py
@@ -23,3 +23,33 @@ class Authentication:
         self.ALGORITHM = algorithm
         self.ACCESS_TOKEN_EXPIRE_MINUTES = access_token_expire_minutes
         self.REFRESH_TOKEN_EXPIRE_MINUTES = refresh_token_expire_minutes
+
+    @staticmethod
+    async def get_token(request: Request):
+        authorization = request.cookies.get("access_token")
+        if authorization is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid or missing token",
+            )
+        # Check if the Authorization header starts with "Bearer "
+        if not authorization.startswith("Bearer "):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid or missing token",
+            )
+
+        # Extract the token part
+        token = authorization.split("Bearer ")[1]
+
+        # Define a regex pattern for a valid token
+        token_pattern = r"^[A-Za-z0-9\-_]+\.([A-Za-z0-9\-_]+)?\.([A-Za-z0-9\-_]+)?$"  # Basic JWT structure
+
+        # Check if the token matches the regex
+        if not re.match(token_pattern, token):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Malformed or invalid token",
+            )
+
+        return token
