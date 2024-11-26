10a1e14426e0cf06e0f20541da160548e68cd43d
hamza-mughal <hamzaxd1123@gmail.com>
2024-11-26 22:27:24 +0500

    Update init file to manage packages

--- schemas/db_schemas/__init__.py
+++ schemas/db_schemas/__init__.py
@@ -1,6 +1,6 @@
-from .users_db_schema import User
-from .foods_db_schema import Food
-from .orders_db_schema import Order
-from .order_items_db_schema import OrderItem
+from .users_db_schema import Users
+from .foods_db_schema import Foods
+from .orders_db_schema import Orders
+from .order_items_db_schema import OrderItems
 
-__all__ = ["User", "Food", "Order", "OrderItem"]
+__all__ = ["Users", "Foods", "Orders", "OrderItems"]
