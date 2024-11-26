0fa3ed616e2b92cc3083d512e81050877ec5f725
hamza-gtechnosoft <hamzaxd1123@gmail.com>
2024-11-26 22:16:48 +0500

    Create authentication handler to handle all functionalities related to security

---
+++ handlers/authentication_handlers.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends, Response
+from fastapi.security import OAuth2PasswordRequestForm
+from utilities.dependencies import db_dependency
+from models.authentication_model import Authentication
+
+authentication = Authentication()
+
+router = APIRouter(tags=["Authentication"])
+
+
+@router.post("/login")
+async def login(
+    db: db_dependency,
+    response: Response,
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+):
+    return await authentication.login(user_credentials, db, response)
