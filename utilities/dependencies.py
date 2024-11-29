c7ba289d79164288449290e6e0856f78aed5a13e
hamza-mughal <hamzaxd1123@gmail.com>
2024-11-29 21:05:34 +0500

    Update 'token_dependency' to also check allowed roles

--- utilities/dependencies.py
+++ utilities/dependencies.py
@@ -5,7 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 db_dependency = Annotated[AsyncSession, Depends(get_db)]
 
+
 from models.authentication_model import Authentication
 authentication = Authentication()
+uncheck_token = Annotated[dict, Depends(authentication.verify_token)]
 
-token_dependency = Annotated[dict, Depends(authentication.verify_token)]
+
+from RBAC.role_based_access_control import check_access
+token_dependency = Annotated[dict, Depends(check_access)]
