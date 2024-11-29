bd48196cf41b483f3a77f8caa83ce396c3ab2db6
hamza-mughal <hamzaxd1123@gmail.com>
2024-11-29 21:02:54 +0500

    Add Restaurant_id FK in foods table

--- schemas/db_schemas/foods_db_schema.py
+++ schemas/db_schemas/foods_db_schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, func
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime, func
 from sqlalchemy.orm import relationship
 from engines.sql_engine import Base
 
@@ -8,8 +8,10 @@ class Foods(Base):
     __tablename__ = "foods"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
 
-    order_items = relationship("OrderItems", back_populates="food", cascade="all, delete-orphan")
+    order_items = relationship("OrderItems", back_populates="foods", cascade="all, delete-orphan")
+    restaurants = relationship("Restaurants", back_populates="foods")
