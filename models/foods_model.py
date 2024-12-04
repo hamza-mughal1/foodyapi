d609bcd7088c1d15ad3e55d6211f75d55a86c5c6
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-04 20:36:13 +0500

    Update 'create food' to only allow restaurants create food
    If the vendor doesn't have a Restaurant then it won't allow him to create the food

--- models/foods_model.py
+++ models/foods_model.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from schemas.db_schemas.foods_db_schema import Foods
+from schemas.db_schemas.restaurants_db_schema import Restaurants
 from schemas.pydantic_schemas import foods_pydantic_schema as food_schema
+from sqlalchemy.future import select
+from fastapi import HTTPException, status
 
 
 class FoodModel:
@@ -8,7 +11,15 @@ class FoodModel:
         pass
 
     async def create_food(self, food_data: food_schema.CreateFood, db: AsyncSession, token):
-        food = Foods(**food_data.model_dump())
+        result = await db.execute(select(Restaurants).where(Restaurants.user_id==token["user_id"]))
+        restaurant = result.scalar_one_or_none()
+        if restaurant is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Restaurant Not Found (First Create A Restaurant)")
+        
+        data = food_data.model_dump()
+        data["restaurant_id"] = restaurant.id
+        
+        food = Foods(**data)
         db.add(food)
         await db.commit()
 
