99513bddababdf9f5abae13dd820cdca0fa4f2f5
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-04 20:38:07 +0500

    Update __init__.py to allow dynamically import schemas

--- schemas/db_schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from .users_db_schema import Users
-from .foods_db_schema import Foods
-from .orders_db_schema import Orders
-from .order_items_db_schema import OrderItems
-
-__all__ = ["Users", "Foods", "Orders", "OrderItems"]
