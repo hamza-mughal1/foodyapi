47b65dad425453f49171dfc46db0c6f1ec0c9807
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-04 20:31:52 +0500

    Add automatic RBAC config generator
    It add all routes in the config file with 'all' as default role. It removes routes if they aren't in api. It creates new config if doesn't exists

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from engines.sql_engine import Base, engine
-from schemas.db_schemas import *
 from sqlalchemy.ext.asyncio import AsyncEngine
 from handlers.users_handler import router as users_router
 from handlers.authentication_handler import router as authentication_router
@@ -9,6 +8,7 @@ from handlers.foods_handler import router as foods_router
 from handlers.orders_handler import router as orders_router
 from handlers.vendors_handler import router as vendors_router
 from handlers.restaurant_handler import router as restaurants_router
+from RBAC.create_RBAC_config import update_routes_json
 
 # create tables in the db if don't exist
 async def create_tables(engine: AsyncEngine):
@@ -22,9 +22,17 @@ async def drop_tables(engine: AsyncEngine):
 # create app
 app = FastAPI()
 
+app.include_router(users_router)
+app.include_router(authentication_router)
+app.include_router(foods_router)
+app.include_router(orders_router)
+app.include_router(vendors_router)
+app.include_router(restaurants_router)
+
 # check for tables creation on the startup
 @app.on_event("startup")
 async def on_startup():
+    update_routes_json(app, file_path=FILE_PATH)
     await create_tables(engine)
     
 @app.get("/drop-tables")
@@ -42,13 +50,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(users_router)
-app.include_router(authentication_router)
-app.include_router(foods_router)
-app.include_router(orders_router)
-app.include_router(vendors_router)
-app.include_router(restaurants_router)
 
 
 # home route
