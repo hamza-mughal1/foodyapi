e873415369604af4dbd9476fda5547fd81b5ba0b
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-05 19:59:39 +0500

    add formatting to main.py

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ from handlers.vendors_handler import router as vendors_router
 from handlers.restaurant_handler import router as restaurants_router
 from RBAC.create_RBAC_config import update_routes_json
 
+
 # create tables in the db if don't exist
 async def create_tables(engine: AsyncEngine):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-# drop tables only for development 
+
+# drop tables only for development
 async def drop_tables(engine: AsyncEngine):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
 
 # create app
 app = FastAPI()
@@ -32,18 +35,21 @@ app.include_router(orders_router)
 app.include_router(vendors_router)
 app.include_router(restaurants_router)
 
+
 # check for tables creation on the startup
 @app.on_event("startup")
 async def on_startup():
-    # update routes config file 
+    # update routes config file
     update_routes_json(app, file_path=FILE_PATH)
     await create_tables(engine)
-    
+
+
 # drop tables endpoint to drop all tables and data in db (only for development)
 @app.get("/drop-tables")
 async def drop_tables_route():
     await drop_tables(engine)
     return {"message": "tables has been dropped!"}
+
 
 # configure CORS middleware (allowed to everyone for now)
 origins = ["*"]
@@ -59,9 +65,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# add token verfication, authentication, and RBAC middlware. Doing it on middlware level before each request so 
+# add token verfication, authentication, and RBAC middlware. Doing it on middlware level before each request so
 # could seperate the logic from endpoint level. Will allow moving to micro-servies easily.
 app.add_middleware(AuthenticationMiddleware)
+
 
 # home route
 @app.get("/")
