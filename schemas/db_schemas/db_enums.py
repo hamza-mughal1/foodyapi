240670d89659b4a2f5a42d55cadee264fe71b4d5
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-06 20:09:43 +0500

    Add enum 'AcceptanceStatus' for 'Orders' table

--- schemas/db_schemas/db_enums.py
+++ schemas/db_schemas/db_enums.py
@@ -10,3 +10,8 @@ class OrderStatus(enum.Enum):
     in_progress = "in_progress"
     done = "done"
     
+# Enum for Order Acceptance Status by Restaurant
+class AcceptanceStatus(enum.Enum):
+    pending = "pending"
+    accepted = "accepted"
+    rejected = "rejected"
