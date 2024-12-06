8570ac71c886f24475eb57dcadfe7a3d5e105390
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-06 20:10:18 +0500

    Add 'order_status' and 'acceptance_status' columns in 'orders' table

--- schemas/db_schemas/orders_db_schema.py
+++ schemas/db_schemas/orders_db_schema.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, Float, DateTime, func
+from sqlalchemy import Column, Enum, ForeignKey, Integer, Float, DateTime, func
 from sqlalchemy.orm import relationship
 from engines.sql_engine import Base
+from schemas.db_schemas import db_enums
 
 # Orders Table
 class Orders(Base):
@@ -9,6 +10,8 @@ class Orders(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     price = Column(Float, nullable=False)
+    order_status = Column(Enum(db_enums.OrderStatus), nullable=False, server_default=db_enums.OrderStatus.in_progress.value)
+    acceptance_status = Column(Enum(db_enums.AcceptanceStatus), nullable=False, server_default=db_enums.AcceptanceStatus.pending.value)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
     
     users = relationship("Users", back_populates="orders")
