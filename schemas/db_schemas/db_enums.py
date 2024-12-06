223f40a60d9a478c646ced27d2a4c4b23fc35058
hamza-mughal <hamzaxd1123@gmail.com>
2024-12-06 20:09:01 +0500

    Add enum 'OrderStatus' for 'Orders' table

--- schemas/db_schemas/db_enums.py
+++ schemas/db_schemas/db_enums.py
@@ -4,3 +4,9 @@ import enum
 class UserRole(enum.Enum):
     user = "user"
     vendor = "vendor"
+    
+# Enum for Order Progress Status
+class OrderStatus(enum.Enum):
+    in_progress = "in_progress"
+    done = "done"
+    
